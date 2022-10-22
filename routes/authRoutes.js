6c40c8abc83f9d4d0b295783716a8a4460b9f1d5
ceydaulubas <ceyda.ulubas@gmail.com>
2022-10-22 23:45:23 +0300

    created logic and authantication of login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,4 +5,6 @@ const router = Router();
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
+// router.post('/welcome', authController.welcome);
+
 module.exports = router;
